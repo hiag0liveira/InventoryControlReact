9145893a90c6748d69d73f17cef35e0848672a41
Hiag0liveira <hiagophd@gmail.com>
2024-10-14 15:35:18 -0300

    Refactor router.jsx to use proper casing for import paths

--- src/router.jsx
+++ src/router.jsx
@@ -1,28 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
-import ItemsLayout from "./pages/items/Layout";
-import ListItems from "./pages/items/ListItems";
-import CreateItem from "./pages/items/CreateItem";
-import ShowItem from "./pages/items/ShowItem";
-import UpdateItem from "./pages/items/UpdateItem";
+import ItemsLayout from "./pages/Items/Layout";
+import ListItems from "./pages/Items/ListItems";
+import CreateItem from "./pages/Items/CreateItem";
+import ShowItem from "./pages/Items/ShowItem";
+import UpdateItem from "./pages/Items/UpdateItem";
 import Home from "./pages/Home";
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+  {
     path: "/",
     element: <RootLayout />,
     children: [
-        { index: true, element: <Home /> },
-        {
-            path: "items",
-            element: <ItemsLayout />,
-            children: [
-                { index: true, element: <ListItems /> },
-                { path: "new", element: <CreateItem /> },
-                { path: ":id", element: <ShowItem /> },
-                { path: ":id/update", element: <UpdateItem /> }
-            ]
-        }
-    ]
-}])
+      { index: true, element: <Home /> },
+      {
+        path: "items",
+        element: <ItemsLayout />,
+        children: [
+          { index: true, element: <ListItems /> },
+          { path: "new", element: <CreateItem /> },
+          { path: ":id", element: <ShowItem /> },
+          { path: ":id/update", element: <UpdateItem /> },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
